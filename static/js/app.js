fadd6d86e5d9e6f0ccb41c5a621bf45dd7e911e9
Greg Wilson <greg@gbwilson.com>
2022-01-24 14:22:13 -0500

    Incremental progress, data is showing now - better save state

--- static/js/app.js
+++ static/js/app.js
@@ -10,13 +10,13 @@ function buildTable(data) {
 
   // Next, loop through each object in the data
   // and append a row and cells for each value in the row
-  data.forEach((dataRow) => {
+  data.forEach(function(dataRow){
     // Append a row to the table body
-    let row = tbody.append("tr");
+    var row = tbody.append("tr");
 
     // Loop through each field in the dataRow and add
     // each value as a table cell (td)
-    Object.values(dataRow).forEach((val) => {
+    Object.values(dataRow).forEach(function(val) {
       let cell = row.append("td");
       cell.text(val);
     });
@@ -27,25 +27,26 @@ function buildTable(data) {
 var filters ={};
 
 // 3. Use this function to update the filters. 
-function updateFilters() {
+function updateFilters(){
 
     // 4a. Save the element that was changed as a variable.
     var changeElement = d3.select(this).select("input");
 
     // 4b. Save the value that was changed as a variable.
-    var elementValue = changeElement.property("value")
+    var elementValue = changeElement.property("value");
 
     // 4c. Save the id of the filter that was changed as a variable.
-    var filterId = changeElement.attr("id)")
+    var filterId = changeElement.attr("id");
   
     // 5. If a filter value was entered then add that filterId and value
     // to the filters list. Otherwise, clear that filter from the filters object.
-    if(elemenetValue){
-      filters[filterId] = elemenetValue;
+    if(elementValue){
+      filters[filterId] = elementValue;
     }
-    else(
+    else{
       delete filters[filterId];
-    )
+    }
+
   
     // 6. Call function to apply all filters and rebuild the table
     filterTable();
@@ -53,11 +54,10 @@ function updateFilters() {
   }
   
   // 7. Use this function to filter the table when data is entered.
-  function filterTable() {
+  function filterTable(){
     
-  
     // 8. Set the filtered data to the tableData.
-    let filteredData() = tableData
+    let filteredData = tableData;
   
     // 9. Loop through all of the filters and keep any data that
     // matches the filter values
